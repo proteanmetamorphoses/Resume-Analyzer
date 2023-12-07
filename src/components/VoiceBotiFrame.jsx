a7fc7a887b2a2f22fa2f75418bfb019eb352e05a
proteanmetamorphoses <steve.kurowski@gmail.com>
2023-12-06 17:19:54 -0700

    Remove extranneous console error message

--- src/components/VoiceBotiFrame.jsx
+++ src/components/VoiceBotiFrame.jsx
@@ -14,10 +14,6 @@ const VoiceBotIframe = () => {
       // Get the user's Firebase authentication token
       getIdToken(auth.currentUser)
         .then((firebaseAuthToken) => {
-          console.log('Firebase Auth Token:', firebaseAuthToken); 
-
-          // Directly set the iframe source to your server endpoint
-          // Update the endpoint as needed to match your server configuration
           const serverEndpoint = `https://voicebot.ispeakwell.ca/get-html?authToken=${firebaseAuthToken}`;
           setIframeSrc(serverEndpoint);
         })
@@ -41,30 +37,3 @@ const VoiceBotIframe = () => {
 };
 
 export default VoiceBotIframe;
-
-/* old code pre-API
-
-import React, { useState, useEffect } from 'react';
-import "./VoiceBotiFrame.css";
-
-const VoiceBotIframe = () => {
-  const [iframeSrc, setIframeSrc] = useState('');
-
-  useEffect(() => {
-    // Set the iframe src once when the component mounts
-    setIframeSrc(`https://www.ispeakwell.ca/?version=${new Date().getTime()}`);
-  }, []);
-
-  return (
-    <div id="wrap">
-      <iframe 
-        id="theBot"
-        src={iframeSrc}
-        title="VoiceBot"
-        allow="microphone"
-      ></iframe>
-    </div>
-  );
-};
-
-export default VoiceBotIframe; */
